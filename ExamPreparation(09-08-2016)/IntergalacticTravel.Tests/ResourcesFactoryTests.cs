6a52b5472e62a5208aefe993d122cbc8ef68b225
Georgi Markov <markov_ood@abv.bg>
2017-10-13 20:57:04 +0300

    -initial commit - skeleton for IntergalacticTravel task - tests for UnitsFactory

---
+++ ExamPreparation(09-08-2016)/IntergalacticTravel.Tests/ResourcesFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticTravel.Tests
+{
+    [TestFixture]
+    public class ResourcesFactoryTests
+    {
+        [Test]
+        public void GetResourcesShouldReturnNewlyCreatedResourcesObjectWithCorrectlySetUpProperties()
+        {
+            // Arrange 
+            const string command = "";
+            var factory = new ResourcesFactory();
+
+            // Act
+            var resourcesObj = factory.GetResources(command);
+
+            // Assert
+
+        }
+    }
+}
---
+++ ExamPreparation(09-08-2016)/IntergalacticTravel.Tests/UnitsFactoryTests.cs
@@ -0,0 +1,73 @@
+﻿using IntergalacticTravel.Exceptions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntergalacticTravel.Tests
+{
+    [TestFixture]
+    public class UnitsFactoryTests
+    {
+        [Test]
+        public void GetUnitShouldReturnNewProcyonUnit_WhenAValidCorrespondingCommandIsPassed()
+        {
+            // Arrange
+            const string validCorrespondingCommand = "create unit Procyon Gosho 1";
+            var factory = new UnitsFactory();
+
+            // Act
+            var unit = factory.GetUnit(validCorrespondingCommand);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(Procyon), unit);
+        }
+
+        [Test]
+        public void GetUnitShouldReturnNewLuytenUnit_WhenAValidCorrespondingCommandIsPassed()
+        {
+            // Arrange
+            const string validCorrespondingCommand = "create unit Luyten Pesho 2";
+            var factory = new UnitsFactory();
+
+            // Act
+            var unit = factory.GetUnit(validCorrespondingCommand);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(Luyten), unit);
+        }
+
+        [Test]
+        public void GetUnitShouldReturnNewLacailleUnit_WhenAValidCorrespondingCommandIsPassed()
+        {
+            // Arrange
+            const string validCorrespondingCommand = "create unit Lacaille Tosho 3";
+            var factory = new UnitsFactory();
+
+            // Act
+            var unit = factory.GetUnit(validCorrespondingCommand);
+
+            // Assert
+            Assert.IsInstanceOf(typeof(Lacaille), unit);
+        }
+        
+        [TestCase("create Luyten Pesho 2")]
+        [TestCase("unit Luyten Pesho 2")]
+        [TestCase("create new unit Luyten Pesho 2")]
+        [TestCase("create Luyten unit Pesho 2")]
+        [TestCase("create unit Pesho 2")]
+        [TestCase("a b c d e")]
+        [TestCase("create unit a b c")]
+        [TestCase("create unit Luyten Pesho a")]
+        public void GetUnitShouldThrowInvalidUnitCreationCommandException_WhenTheCommandPassedIsNotInTheValidFormat(string command)
+        {
+            // Arrange
+            var factory = new UnitsFactory();
+
+            // Act & Assert
+            Assert.Throws<InvalidUnitCreationCommandException>(() => factory.GetUnit(command));
+        }
+    }
+}
