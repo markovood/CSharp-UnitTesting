1b7dd214a16f7c31d8281d7ffb94b983316e2343
Georgi Markov <markov_ood@abv.bg>
2017-10-13 23:37:59 +0300

    added ResourcesFactory tests

--- ExamPreparation(09-08-2016)/IntergalacticTravel.Tests/ResourcesFactoryTests.cs
+++ ExamPreparation(09-08-2016)/IntergalacticTravel.Tests/ResourcesFactoryTests.cs
@@ -10,18 +10,51 @@ namespace IntergalacticTravel.Tests
     [TestFixture]
     public class ResourcesFactoryTests
     {
-        [Test]
-        public void GetResourcesShouldReturnNewlyCreatedResourcesObjectWithCorrectlySetUpProperties()
+        [TestCase("create resources gold(20) silver(30) bronze(40)")]
+        [TestCase("create resources gold(20) bronze(40) silver(30)")]
+        [TestCase("create resources silver(30) bronze(40) gold(20)")]
+        [TestCase("create resources silver(30) gold(20) bronze(40)")]
+        [TestCase("create resources bronze(40) gold(20) silver(30)")]
+        [TestCase("create resources bronze(40) silver(30) gold(20)")]
+        public void GetResourcesShouldReturn_NewlyCreatedResourcesObject_WithCorrectlySetUpProperties_NoMatterWhatTheOrderOfParametersIs(string command)
         {
             // Arrange 
-            const string command = "";
+            const uint expectedBronzeCoins = 40;
+            const uint expectedSilverCoins = 30;
+            const uint expectedGoldCoins = 20;
             var factory = new ResourcesFactory();
 
             // Act
             var resourcesObj = factory.GetResources(command);
 
             // Assert
+            Assert.AreEqual(expectedBronzeCoins, resourcesObj.BronzeCoins);
+            Assert.AreEqual(expectedSilverCoins, resourcesObj.SilverCoins);
+            Assert.AreEqual(expectedGoldCoins, resourcesObj.GoldCoins);
+        }
 
+        [TestCase("create resources x y z")]
+        [TestCase("tansta resources a b")]
+        [TestCase("absolutelyRandomStringThatMustNotBeAValidCommand")]
+        public void GetResourcesShouldThrow_InvalidOperationException_WhenTheInputStringRepresentsInvalidCommand(string command)
+        {
+            // Arrange
+            var factory = new ResourcesFactory();
+
+            // Act & Assert
+            Assert.Throws<InvalidOperationException>(() => factory.GetResources(command));
+        }
+
+        [TestCase("create resources silver(10) gold(97853252356623523532) bronze(20)")]
+        [TestCase("create resources silver(555555555555555555555555555555555) gold(97853252356623523532999999999) bronze(20)")]
+        [TestCase("create resources silver(10) gold(20) bronze(4444444444444444444444444444444444444)")]
+        public void GetResourcesShouldThrow_OverflowException_WhenTheInputStringCommandIsInTheCorrectFormat_ButAnyOfTheValuesThatRepresentTheResourceAmountIsLargerThanUintMaxValue(string command)
+        {
+            // Arrange 
+            var factory = new ResourcesFactory();
+
+            // Act & Assert
+            Assert.Throws<OverflowException>(() => factory.GetResources(command));
         }
     }
 }
